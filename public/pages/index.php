61935a754a18842ef45d7c4fc1e9b4418e88a107
DYLANeay <dylan.eray6@gmail.com>
2025-10-21 10:25:00 +0200

    create a page for dashboard page layout and content

---
+++ public/pages/index.php
@@ -0,0 +1,32 @@
+<?php
+
+require __DIR__ . '/../../src/utils/autoloader.php';
+// Page de dashboard ou page d'accueil après connexion
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="color-scheme" content="light dark">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+
+    <title>Dashboard | RunTracker</title>
+</head>
+
+<body>
+    <main class="container">
+        <h1>Dashboard</h1>
+
+        <h3>Bienvenue sur le dashboard de l'application web RunTracker.</h3>
+
+        <h4>L'application parfaite pour suivre votre progression en course à pied</h4>
+
+    </main>
+</body>
+
+</html>
