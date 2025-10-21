08a24440934869c372da96180c487930eeb4d469
DYLANeay <dylan.eray6@gmail.com>
2025-10-21 10:35:04 +0200

    create: add course creation page layout and content

---
+++ public/pages/create.php
@@ -0,0 +1,37 @@
+<?php
+
+require __DIR__ . '/../../src/utils/autoloader.php';
+// Page de création d'une nouvelle course
+
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="color-scheme" content="light dark">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
+
+    <title>Ajouter une course | RunTracker</title>
+</head>
+
+<body>
+    <main class="container">
+        <h1>Ajouter une course</h1>
+
+        <h3>Bienvenue sur la page de création d'une nouvelle course de l'application web RunTracker.</h3>
+
+        <h4>L'application parfaite pour suivre votre progression en course à pied</h4>
+
+
+        <button><a href="./index.php">Voir mon dashboard</a></button>
+
+        <button><a href="./progress.php">Voir ma progression</a></button>
+
+    </main>
+</body>
+
+</html>
