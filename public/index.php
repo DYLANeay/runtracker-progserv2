c11afb0e7d11999b880941e5e48985130be08a91
DYLANeay <dylan.eray6@gmail.com>
2025-10-21 10:24:06 +0200

    create a first home page for the app

---
+++ public/index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="color-scheme" content="light dark">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Page d'accueil | RunTracker</title>
+</head>
+
+<body>
+    <main class="container">
+        <h1>Page d'accueil</h1>
+
+        <h3>Bienvenue sur la page d'accueil de l'application web RunTracker.</h3>
+
+        <h4>L'application parfaite pour suivre votre progression en course à pied</h4>
+
+
+        <p><a href="./public/pages/index.php"><button>Aller à la gestion des courses</button></a></p>
+    </main>
+</body>
+
+</html>
