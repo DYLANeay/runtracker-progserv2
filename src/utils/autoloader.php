84d5d65c9b7c6e620bad542b01bb233937e7fbac
DYLANeay <dylan.eray6@gmail.com>
2025-10-21 10:23:03 +0200

    fix autoloader function to accept class name parameter and correct file path checks

--- src/utils/autoloader.php
+++ src/utils/autoloader.php
@@ -1,15 +1,15 @@
 <?php
 // Charge les classes automatiquement
-spl_autoload_register(function () {
+spl_autoload_register(function ($className) {
     // Convertit les séparateurs de namespace en séparateurs de répertoires
-     = str_replace('\', '/', );
+    $classPath = str_replace('\\', '/', $className);
 
     // Construit le chemin complet du fichier
-     = __DIR__ . '/../classes/' .  . '.php';
+    $filePath = __DIR__ . '/../classes/' . $classPath . '.php';
 
     // Vérifie si le fichier existe avant de l'inclure
-    if (file_exists()) {
+    if (file_exists($filePath)) {
         // Inclut le fichier de classe
-        require_once ;
+        require_once $filePath;
     }
 });
