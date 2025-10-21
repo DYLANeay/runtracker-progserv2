01d7e20b09fe2bceec1015effda68161084d2045
DYLANeay <dylan.eray6@gmail.com>
2025-10-21 10:35:20 +0200

    add progress page layout and content

---
+++ public/pages/progress.php
@@ -0,0 +1,37 @@
+<?php
+
+// Retrieve data from database
+require __DIR__ . '/../../src/utils/autoloader.php';
+
+// Here comes the js code to style data into graphs, ...
+
+?>
+
+
+
+<!-- Show data -->
+<!DOCTYPE html>
+<html lang="fr">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="color-scheme" content="light dark">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@picocss/pico@2/css/pico.min.css">
+    <link rel="stylesheet" href="assets/css/style.css">
+
+    <title>Page de progression | RunTracker</title>
+</head>
+
+<body>
+    <main class="container">
+        <h1>Page de progression</h1>
+
+        <h3>Bienvenue sur la page de progression de l'application web RunTracker.</h3>
+
+        <h4></h4>
+
+    </main>
+</body>
+
+</html>
