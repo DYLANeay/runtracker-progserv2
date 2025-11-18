9fd88c42ba8bd35aedc80f83af8ad4109453af14
DYLANeay <dylan.eray6@gmail.com>
2025-11-18 14:38:02 +0100

    solve little issue on the path on a button

--- public/pages/login.php
+++ public/pages/login.php
@@ -93,7 +93,7 @@ if ($_SERVER["REQUEST_METHOD"] === "POST") {
         <p><small>Pas encore de compte ? <a href="register.php">Inscrivez-vous ici</a></small></p>
         
         <br>
-        <button><a href="../index.php"><?= t('back_to_home') ?></a></button>
+        <button><a href="../../index.php"><?= t('back_to_home') ?></a></button>
     </main>
 
     <?php include __DIR__ . '/../../src/i18n/language-footer.php'; ?>
